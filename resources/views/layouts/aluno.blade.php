0a2c8435b0cb32556471e2bb46ef4a133c937597
Jeferson Gomes <67127121+jeferson3@users.noreply.github.com>
2022-06-14 20:24:56 -0300

    layouts

--- resources/views/layouts/aluno.blade.php
+++ resources/views/layouts/aluno.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Gestão Escolar - Autenticação</title>
+        <title>Gestão Escolar - Dashboard</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -14,32 +14,22 @@
         @yield('styles')
     </head>
     <body>
-        <div class="container-login">
-            <div class="row">
-                <div class="col-md-12 bg-gray-100" style="width: 50vw">
-                    <div class="row">
-                        <div class="col-md-8 p-5 border-end">
-                            @yield('content')
-                        </div>
-                        <div class="col-md-4 align-self-center">
-                            <ul class="menu-lateral-login">
-                               <li>
-                                   <a href="{{route('auth.aluno.login.index')}}">
-                                       <i class="fas fa-user-graduate"></i>
-                                       Área alunos
-                                   </a>
-                               </li>
-                               <li>
-                                   <a href="{{route('auth.professor.login.index')}}">
-                                        <i class="fas fa-user-tie"></i>
-                                       Área docentes
-                                   </a>
-                               </li>
-                            </ul>
-                        </div>
-                    </div>
+        <header>
+            <nav class="navbar navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand">Gestão Escolar - Área do aluno</a>
+                    <form class="d-flex" method="post" action="{{route('auth.aluno.logout')}}">
+                        @csrf
+                        <button class="btn btn-outline-danger" type="submit">
+                            <i class="fas fa-sign-out"></i>
+                            Sair
+                        </button>
+                    </form>
                 </div>
-            </div>
+            </nav>
+        </header>
+        <div class="container p-5">
+            @yield('content')
         </div>
     </body>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
